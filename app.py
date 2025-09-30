56b6fdf4e7847c5aeed8dd6f1444b2c212f21d30
Aayush Kumar Gupta <aayushgupta120305@gmail.com>
2025-09-30 23:31:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def company_news():
   },
   "Stripe": {
     "Role": "Software Engineering New Grad",
-    "Stipend": "₹10L - ₹15L LPA",
+    "Stipend": "₹12L - ₹15L LPA",
     "Loc": "Bengaluru",
     "Link": "https://stripe.com/careers",
     "Batch": "2026",
@@ -161,14 +161,6 @@ def company_news():
     "Batch": "2023 to 2025",
     "Logo": "https://img.logo.dev/infineon.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Stripe": {
-    "Role": "Software Engineering New Grad",
-    "Stipend": "₹12L - ₹15L LPA",
-    "Loc": "Bengaluru",
-    "Link": "https://stripe.com/careers",
-    "Batch": "2026",
-    "Logo": "https://img.logo.dev/stripe.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
   "Harmonic": {
     "Role": "Associate QA Engineer",
     "Stipend": "₹4L - ₹6L LPA",
@@ -193,231 +185,80 @@ def company_news():
     "Batch": "2026",
     "Logo": "https://img.logo.dev/uber.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Mindtickle": {
-    "Role": "Software Engineer Internship",
-    "Stipend": "₹70,000/month",
+  "Autodesk": {
+    "Role": "Software Dev Engineer",
+    "Stipend": "₹8–12 LPA",
+    "Loc": "Bengaluru, India",
+    "Link": "https://www.autodesk.com/careers/overview",
+    "Date_of_publish": "29 September 2025",
+    "Batch": "2023, 2024, 2025",
+    "Logo": "https://img.logo.dev/autodesk.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+  },
+  "Verto": {
+    "Role": "Associate Software Engineer",
+    "Stipend": "₹5–10 LPA",
     "Loc": "Pune, India",
-    "Link": "https://www.linkedin.com/jobs/view/4305566133",
-    "Batch": "2026/27 passout",
-    "Logo": "https://img.logo.dev/mindtickle.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+    "Link": "https://www.vertofx.com/About-Us/Company/Careers/",
+    "Date_of_publish": "29 September 2025",
+    "Batch": "2024, 2025",
+    "Logo": "https://img.logo.dev/vertofx.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Sense": {
-    "Role": "Software Engineering Internship",
-    "Stipend": "₹50,000/month",
-    "Loc": "India",
-    "Link": "https://sensehr.sensehq.com/careers/jobs/175",
-    "Batch": "2026/27 passouts",
-    "Logo": "https://img.logo.dev/sensehq.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+  "Fortrea_Web": {
+    "Role": "Website Designer",
+    "Stipend": "₹3–5 LPA",
+    "Loc": "Bangalore, India",
+    "Link": "https://careers.fortrea.com/",
+    "Date_of_publish": "29 September 2025",
+    "Batch": "2023, 2024, 2025",
+    "Logo": "https://img.logo.dev/fortrea.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Rippling": {
-    "Role": "SDE (Frontend)",
-    "Stipend": "₹66.5 LPA",
-    "Loc": "",
-    "Link": "https://ats.rippling.com/en-GB/rippling/jobs/fce552a3-bcae-4f35-b5ff-0143031a5640",
-    "Batch": "2022/23/24 passouts",
-    "Logo": "https://img.logo.dev/rippling.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+  "Fortrea_SEO": {
+    "Role": "SEO Apprentice",
+    "Stipend": "₹3–4 LPA",
+    "Loc": "Bangalore, India",
+    "Link": "https://careers.fortrea.com/",
+    "Date_of_publish": "30 September 2025",
+    "Batch": "2023, 2024, 2025",
+    "Logo": "https://img.logo.dev/fortrea.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Acko": {
-    "Role": "Analytics Internship",
-    "Stipend": "Not specified",
-    "Loc": "Bangalore",
-    "Link": "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdZiQ8KrHnsdvzJujqsXvPwoHapvpReh4wtXSImBcN1UYfGCg/viewform?usp=send_form&pli=1",
-    "Batch": "2025/26 passouts",
-    "Logo": "https://img.logo.dev/acko.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+  "Newfold Digital": {
+    "Role": "AI Engineer Intern",
+    "Stipend": "₹25,000–42,000/month",
+    "Loc": "Mumbai, India",
+    "Link": "https://www.newfold.com/careers",
+    "Date_of_publish": "30 September 2025",
+    "Batch": "2023, 2024, 2025",
+    "Logo": "https://img.logo.dev/newfold.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "Qualcomm": {
-    "Role": "Associate Engineer",
-    "Stipend": "Not specified",
-    "Loc": "Bangalore, Chennai",
-    "Link": "https://careers.qualcomm.com/careers/job/446706882405",
-    "Batch": "2026 passouts",
-    "Logo": "https://img.logo.dev/qualcomm.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Meta": {
-    "Role": "Multiple Software and Product Roles",
-    "Stipend": "$110K-$200K",
-    "Loc": "Bangalore",
-    "Link": "https://www.metacareers.com/jobs?offices%5B0%5D=Bangalore%2C%20India&page=2",
-    "Batch": "2022/23/24 passouts and before",
-    "Logo": "https://img.logo.dev/meta.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Waymo": {
-    "Role": "Multiple ML/AI Roles",
-    "Stipend": "$190K-$340K US market",
-    "Loc": "Bangalore",
-    "Link": "https://careers.withwaymo.com/jobs/search?address%5B%5D=12.974%2C77.701",
-    "Batch": "2021/22/23/24 passouts and before",
-    "Logo": "https://img.logo.dev/waymo.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Youtube": {
+  "Teradata": {
     "Role": "Software Engineer",
-    "Stipend": "Not specified",
-    "Loc": "India",
-    "Link": "https://www.google.com/about/careers/applications/jobs/results/109851367958291142-software-engineer/",
-    "Batch": "2022/23/24 passouts",
-    "Logo": "https://img.logo.dev/youtube.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+    "Stipend": "₹6–10 LPA",
+    "Loc": "Hyderabad, India",
+    "Link": "https://careers.teradata.com/jobs/219278/software-engineer",
+    "Date_of_publish": "30 September 2025",
+    "Batch": "2022, 2023, 2024, 2025",
+    "Logo": "https://img.logo.dev/teradata.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "JFrog": {
-    "Role": "Internship (Software)",
-    "Stipend": "Not specified",
-    "Loc": "India",
-    "Link": "https://join.jfrog.com/job/7247322-intern/?gh_src=526d35e41us",
-    "Batch": "2025/26 passouts",
-    "Logo": "https://img.logo.dev/jfrog.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "JP Morgan Chase": {
-    "Role": "SDE-1",
-    "Stipend": "Not specified",
-    "Loc": "Banglore",
-    "Link": "https://jpmc.fa.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/job/210667642",
-    "Batch": "2023/24 passouts",
-    "Logo": "https://img.logo.dev/jpmorganchase.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Google Associate Product Manager": {
-    "Role": "Associate Product Manager",
-    "Stipend": "Not specified",
-    "Loc": "India",
-    "Link": "https://www.google.com/about/careers/applications/jobs/results/75038123993506502-associate-product-manager/",
-    "Batch": "2025/26 passouts",
-    "Logo": "https://img.logo.dev/google.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Google Software Engineer Uni Grad": {
-    "Role": "Software Engineer - Uni Grad",
-    "Stipend": "Not specified",
-    "Loc": "India",
-    "Link": "https://www.google.com/about/careers/applications/jobs/results/125611950166942406-software-engineer/",
-    "Batch": "2026 passouts",
-    "Logo": "https://img.logo.dev/google.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Google Application Engineering Internship": {
-    "Role": "Application Engineering Internship - 6 Months",
-    "Stipend": "Not specified",
-    "Loc": "India",
-    "Link": "https://www.google.com/about/careers/applications/jobs/results/88011077542912710-application-engineering-intern-winter-2026",
-    "Batch": "2026 passouts",
-    "Logo": "https://img.logo.dev/google.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Uber Freight": {
-    "Role": "SDE",
-    "Stipend": "Not specified",
-    "Loc": "",
-    "Link": "https://www.uberfreight.com/careers/?gh_jid=4750841008#opportunities",
-    "Batch": "2022/23/24 passouts",
-    "Logo": "https://img.logo.dev/uberfreight.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Rubrik": {
-    "Role": "Software Engineer - Winter Intern",
-    "Stipend": "₹1.5 Lakh",
-    "Loc": "Gurugram",
-    "Link": "https://www.rubrik.com/company/careers/departments/job.7208329.1929",
-    "Batch": "2026 passouts",
-    "Logo": "https://img.logo.dev/rubrik.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Walmart Global Tech": {
-    "Role": "Internship",
-    "Stipend": "₹1 Lakh/m intern stipend; 26 LPA FTE",
-    "Loc": "",
-    "Link": "https://yt.openinapp.co/lhdte",
-    "Batch": "Not specified",
-    "Logo": "https://img.logo.dev/walmart.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Mindtickle": {
-    "Role": "Software Engineer Internship",
-    "Stipend": "₹70,000/month",
-    "Loc": "Pune, India",
-    "Link": "https://www.linkedin.com/jobs/view/4305566133",
-    "Batch": "2026/27 passout",
-    "Logo": "https://img.logo.dev/mindtickle.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Sense": {
-    "Role": "Software Engineering Internship",
-    "Stipend": "₹50,000/month",
-    "Loc": "India",
-    "Link": "https://sensehr.sensehq.com/careers/jobs/175",
-    "Batch": "2026/27 passouts",
-    "Logo": "https://img.logo.dev/sensehq.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Acko": {
-    "Role": "Analytics Internship",
-    "Stipend": "₹30,000/month",
-    "Loc": "Bangalore, India",
-    "Link": "https://docs.google.com/forms/u/0/d/e/1FAIpQLSdZiQ8KrHnsdvzJujqsXvPwoHapvpReh4wtXSImBcN1UYfGCg/viewform?usp=send_form&pli=1",
-    "Batch": "2025/26 passouts",
-    "Logo": "https://img.logo.dev/acko.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Qualcomm": {
-    "Role": "Associate Engineer",
-    "Stipend": "₹60,000/month",
-    "Loc": "Bangalore, India",
-    "Link": "https://careers.qualcomm.com/careers/job/446706882405",
-    "Batch": "2026 passouts",
-    "Logo": "https://img.logo.dev/qualcomm.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Meta": {
-    "Role": "Multiple Software Engineering and Product Roles",
-    "Stipend": "₹1,00,000 - ₹1,25,000/month",
-    "Loc": "Bangalore, India",
-    "Link": "https://www.metacareers.com/jobs?offices%5B0%5D=Bangalore%2C%20India&page=2",
-    "Batch": "2022/23/24 passouts and before",
-    "Logo": "https://img.logo.dev/meta.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Waymo": {
-    "Role": "Multiple ML/AI roles",
-    "Stipend": "₹1,20,000 - ₹1,50,000/month",
-    "Loc": "Bangalore, India",
-    "Link": "https://careers.withwaymo.com/jobs/search?address%5B%5D=12.974%2C77.701",
-    "Batch": "2021/22/23/24 passouts and before",
-    "Logo": "https://img.logo.dev/waymo.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Youtube": {
+  "v4C.ai": {
     "Role": "Software Engineer",
-    "Stipend": "₹90,000 - ₹1,10,000/month",
-    "Loc": "India",
-    "Link": "https://www.google.com/about/careers/applications/jobs/results/109851367958291142-software-engineer/",
-    "Batch": "2022/23/24 passouts",
-    "Logo": "https://img.logo.dev/youtube.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+    "Stipend": "₹5.2 LPA",
+    "Loc": "Remote, India",
+    "Link": "https://www.v4c.ai/careers",
+    "Date_of_publish": "30 September 2025",
+    "Batch": "2024, 2025",
+    "Logo": "https://img.logo.dev/v4c.ai?token=pk_djKZ3gIOQqyja8btgxBpBA"
   },
-  "JFrog": {
-    "Role": "Software Internship",
-    "Stipend": "₹40,000/month",
+  "ShareChat": {
+    "Role": "SDE Intern",
+    "Stipend": "₹50,000/month",
     "Loc": "Bangalore, India",
-    "Link": "https://join.jfrog.com/job/7247322-intern/?gh_src=526d35e41us",
-    "Batch": "2025/26 passouts",
-    "Logo": "https://img.logo.dev/jfrog.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "JP Morgan Chase": {
-    "Role": "SDE-1",
-    "Stipend": "₹80,000/month",
-    "Loc": "Multiple locations, India",
-    "Link": "https://jpmc.fa.oraclecloud.com/hcmUI/CandidateExperience/en/sites/CX_1001/job/210667642",
-    "Batch": "2023/24 passouts",
-    "Logo": "https://img.logo.dev/jpmorganchase.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Microsoft SDE 1": {
-    "Role": "SDE 1",
-    "Stipend": "₹1,00,000/month",
-    "Loc": "Remote",
-    "Link": "https://jobs.careers.microsoft.com/global/en/job/1863345/Software-Engineer",
-    "Batch": "2023/24 passouts",
-    "Logo": "https://img.logo.dev/microsoft.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Walmart Global Tech": {
-    "Role": "Internship",
-    "Stipend": "₹1 Lakh/m intern stipend; Package 26 LPA FTE",
-    "Loc": "India",
-    "Link": "https://yt.openinapp.co/lhdte",
-    "Batch": "Not specified",
-    "Logo": "https://img.logo.dev/walmart.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
-  },
-  "Uber Freight": {
-    "Role": "SDE",
-    "Stipend": "₹80,000/month",
-    "Loc": "India",
-    "Link": "https://www.uberfreight.com/careers/?gh_jid=4750841008#opportunities",
-    "Batch": "2022/23/24 passouts",
-    "Logo": "https://img.logo.dev/uberfreight.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
+    "Link": "https://sharechat.com/careers",
+    "Date_of_publish": "30 September 2025",
+    "Batch": "2025, 2026",
+    "Logo": "https://img.logo.dev/sharechat.com?token=pk_djKZ3gIOQqyja8btgxBpBA"
   }
 }
+
     return jsonify(data)
 
 
