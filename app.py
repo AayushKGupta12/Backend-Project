1eb4dc6abbc9d3b11b55adbfac3c8267548beda2
Aayush Kumar Gupta <aayushgupta120305@gmail.com>
2025-10-01 15:15:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": [
     "http://localhost:5175",
     "http://localhost:5174",
-    "http://localhost:5173"
+    "https://stackbit.vercel.app/"
 ]}})
 
 
